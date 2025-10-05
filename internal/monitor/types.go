f90a2a7775ba7adc5f7d2b27dc1440a8d873b579
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-10-05 20:54:00 +0700

    [monitor] track last error and probe timestamp

--- internal/monitor/types.go
+++ internal/monitor/types.go
@@ -2,14 +2,16 @@ package monitor
 
 import (
 	"context"
-)
-
-const (
-	ServiceOnline  ServiceState = "online"
-	ServiceOffline ServiceState = "offline"
+	"time"
 )
 
 type ServiceState string
+
+const (
+	ServiceStateUnknown ServiceState = "unknown"
+	ServiceStateOnline  ServiceState = "online"
+	ServiceStateOffline ServiceState = "offline"
+)
 
 type Probeer interface {
 	Probe(context.Context) error
@@ -23,8 +25,22 @@ type ServiceStatus struct {
 }
 
 type state struct {
-	Probes int
-	Online bool
+	Probes    int
+	Online    bool
+	Error     error
+	Timestamp time.Time
+}
+
+func (s state) State() ServiceState {
+	if s.Timestamp.IsZero() {
+		return ServiceStateUnknown
+	}
+
+	if s.Online {
+		return ServiceStateOnline
+	}
+
+	return ServiceStateOffline
 }
 
 type ProbesChannel chan error
